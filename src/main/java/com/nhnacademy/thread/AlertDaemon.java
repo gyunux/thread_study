1a2c742008d61d4a86ace65eec90b1106d507e46
dkyun97 <67477254+dkyun97@users.noreply.github.com>
2025-08-03 20:04:00 +0900

    step1-1-daemon-thread

--- src/main/java/com/nhnacademy/thread/AlertDaemon.java
+++ src/main/java/com/nhnacademy/thread/AlertDaemon.java
@@ -1,12 +1,15 @@
 package com.nhnacademy.thread;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class AlertDaemon extends Thread {
 
     public AlertDaemon() {
         //TODO#1 - setDaemon() 메서드를 이용해서 daemon thread로 설정
-
+        setDaemon(true);
         //TODO#2 - Thread 이름을 alert-daemon으로 설정
-
+        setName("alert-daemon");
 
         // ShutdownHook: JVM이 종료되기 직전에 실행되는 Thread
         // 프로그램의 정상/비정상 종료 시 필요한 리소스 정리나 로그 기록 등을 처리하며,
@@ -14,6 +17,7 @@ public class AlertDaemon extends Thread {
         Runtime.getRuntime().addShutdownHook(
                 new Thread(() -> {
                     //TODO#3 AlertDaemon Thread가 종료 시점에 적절한 메시지를 출력합니다.
+                    log.debug("Thread 종료");
                 })
         );
     }
@@ -21,6 +25,16 @@ public class AlertDaemon extends Thread {
     @Override
     public void run() {
         //TODO#4 1초에 한 번씩 Alert Daemon message를 출력 합니다.
+        while(true){
+            try{
+                Thread.sleep(1000);
+                log.debug("Alert Daemon message");
+            } catch(InterruptedException e){
+                throw new RuntimeException(e);
+            }
+        }
+
+
     }
 
 }
